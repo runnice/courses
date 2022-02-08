6a5f60404220f3bd5cd7a5669488cce47449e3e6
Vinicius Oliveira <runnice@gmail.com>
2022-02-08 19:48:48 -0300

    adding new project

---
+++ angular/ByteBank/src/app/nova-transferencia/nova-transferencia.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-nova-transferencia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['./nova-transferencia.component.scss'],
+})
+
+export class NovaTransferenciaComponent {
+  valor: number;
+  destino: number;
+
+  transferir(){
+    console.log('Solicitado nova transferencia');
+    console.log('valor:', this.valor);
+    console.log('destino:', this.destino)
+  }
+}

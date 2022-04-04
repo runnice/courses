7b921a39781e20260b526759e21229d2d4c31eed
Vinicius Oliveira <runnice@gmail.com>
2022-04-04 15:00:58 -0300

    adding new features

--- projeto-byte-bank/SistemaAutenticacao.js
+++ projeto-byte-bank/SistemaAutenticacao.js
@@ -1,11 +1,10 @@
 export class SistemaAutenticacao{
     static login(autenticavel, senha){
-
         if (SistemaAutenticacao.ehAutneticavel(autenticavel)){
         return autenticavel.autenticar(senha);
-    }
-    return false;
-    }
+        }
+        return false;
+        }
 
     static ehAutneticavel(autenticavel){
         return "autenticar" in autenticavel && autenticavel.autenticar instanceof Function;
